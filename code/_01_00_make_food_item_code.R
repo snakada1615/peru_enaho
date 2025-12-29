c368e9139b33ebf73c88d3323d4543ea98f5e6fd
nakada shunichi <khf04440@nifty.com>
2025-12-29 23:42:18 +0900

    食品一覧を新たに作り直し（不一致の値が多かったため）

--- code/_01_00_make_food_item_code.R
+++ code/_01_00_make_food_item_code.R
@@ -1,3 +1,5 @@
+# Description: ENAHO各年の食料支出項目コードとラベルの対応表を作成し、Excelに保存する
+
 
 rm(list = ls(all = TRUE))
 
@@ -31,32 +33,42 @@ for (year_current in yearlist) {
     next
   }
   
-  # 1) 元ラベル属性からコード→説明ラベルのマップを作る（あれば）
-  p <- df_mod7$P601A
-  lab_attr <- attr(p, "labels")
-  
+  if (is.null(df_mod7$P601X)) {
+    p <- df_mod7$P601A
+    lab_attr <- attr(p, "labels")
   if (!is.null(lab_attr)) {
-    # lab_attr: named vector where names = descriptive labels, values = code strings or numbers
-    # 正規化して data.frame にする
-    lab_values <- as.character(unname(lab_attr))
-    lab_names  <- names(lab_attr)
-    map_attr <- tibble(
-      code_raw = trimws(lab_values),
-      label_text = lab_names,
-      year = year_current
-    ) %>%
+      # lab_attr: named vector where names = descriptive labels, values = code strings or numbers
+      # 正規化して data.frame にする
+      lab_values <- as.character(unname(lab_attr))
+      lab_names  <- names(lab_attr)
+      map_attr <- tibble(
+        code_raw = trimws(lab_values),
+        label_text = lab_names,
+        year = year_current
+      ) %>%
       mutate(code_num = suppressWarnings(as.numeric(code_raw)))
+    } else {
+      map_attr <- tibble(
+        code_raw = character(), 
+        label_text = character(), 
+        year = character(),
+        code_num = numeric(code_raw)
+        )
+    }
+    res <- bind_rows(res, map_attr)
   } else {
-    map_attr <- tibble(code_raw = character(), label_text = character(), year = character())
+    res_temp <- df_mod7 %>%
+      mutate(year = year_current) %>%
+      select(P601A, P601X, year) %>%
+      distinct() %>%
+      mutate(
+        code_raw = as.character(haven::zap_labels(P601A)),
+        label_text = as.character(P601X),
+        code_num = suppressWarnings(as.numeric(code_raw))
+      ) %>% 
+      select(code_raw, label_text, year, code_num)
+    res <- bind_rows(res, res_temp )
   }
-  
-  res <- bind_rows(res, map_attr)
-  
-  # write.xlsx(map_attr,
-  #   file = file.path(save_folder, paste0("label_map_food_item_", year_current, ".xlsx")),
-  #            sheetName = "label_map_food_item",
-  #            overwrite = TRUE)
-  # cat("  Saved label map for year", year_current, "\n")  
 }
 
 # 全年分をまとめて保存
